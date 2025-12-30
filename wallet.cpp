7ff28e0f76a98d92da9031036339f95f86b35274
Segun Oladiran <sheghunoladiran9@gmail.com>
2025-12-30 21:14:33 +0100

    implemented fulfil order

--- wallet.cpp
+++ wallet.cpp
@@ -1,6 +1,8 @@
 #include "wallet.h"
 #include <iostream>
 
+#include "csv_reader.h"
+
 wallet::wallet() {}
 
 void wallet::insert_currency(std::string type, double amount) {
@@ -49,4 +51,22 @@ std::string wallet::to_string() {
     }
 
     return s;
+}
+
+bool wallet::can_fulfill_order(const order_book_entry &order) {
+    std::vector<std::string> currs = csv_reader::tokenise(order.product, '/');
+    // ask
+    if (order.order_type == order_book_type::ask) {
+        const double amount = order.amount;
+        const std::string currency = currs[0];
+        return this->contains_currency(currency, amount);
+    }
+
+    // bid
+    if (order.order_type == order_book_type::bid) {
+        const double amount = order.amount * order.price;
+        const std::string currency = currs[1];
+        return this->contains_currency(currency, amount);
+    }
+    return false;
 }
--- wallet.h
+++ wallet.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include <string>
+#include "order_book_entry.h"
 
 class wallet {
 public:
@@ -8,6 +9,8 @@ public:
     void insert_currency(std::string type, double amount);
     bool remove_currency(std::string type, double amount);
     bool contains_currency(std::string type, double amount);
+    bool can_fulfill_order(const order_book_entry &order);
+
     std::string to_string();
 
 private:
